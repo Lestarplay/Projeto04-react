2b74027f4565f494758e822e653c23345ba412ed
Breno B.franco <162745949+Lestarplay@users.noreply.github.com>
2024-10-28 20:56:06 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Conversordemoeda from './assets/components/Conversordemoeda'; 
+function App() {
+  return (
+    <div className="App">
+      <Conversordemoeda />
+    </div>
+  );
+}
+
+export default App;
